e3e2ab9638d2fd6fa7762f13a72a5034476eef72
Sergio Brito <sergiobritor@gmail.com>
2022-01-30 12:28:30 -0500

    test(03-01): try wallaby features

--- simple.spec.js
+++ simple.spec.js
@@ -2,3 +2,16 @@ it('should enable the feature', () => {
   const featureEnable = true;
   expect(featureEnable).toBe(true);
 });
+
+function add(a, b) {
+  return a + b;
+}
+
+it('should behave add two numbers', () => {
+  // Arrange
+  const expectedResult = 10;
+  // Act
+  const result = add(2, 8);
+  // Assert
+  expect(result).toBe(expectedResult);
+});
