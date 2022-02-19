162e2a65d42ef6c195c692443525b243534a804b
Sergio Brito <sergiobritor@gmail.com>
2022-02-19 00:07:53 -0500

    feat(04-01): define eslint rules

--- libs/menu/src/lib/dishes/dishes.component.ts
+++ libs/menu/src/lib/dishes/dishes.component.ts
@@ -6,7 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dishes.component.scss'],
 })
 export class DishesComponent implements OnInit {
-  constructor() {}
+  ngOnInit(): void {
+    const test = 'Hola Mundo';
 
-  ngOnInit(): void {}
+    console.log(test);
+  }
+
+  getDishes(
+    type: string,
+    count: number,
+    taste: string,
+    inStock: boolean
+  ): string {
+    let selection = '';
+
+    if (type === 'vegan' && count > 2 && taste !== 'spicy') {
+      selection = `${count} ${type} dishes, no spicy`;
+    }
+    this.validate(inStock);
+
+    return selection;
+  }
+
+  validate(inStock: boolean) {
+    if (inStock) {
+      console.log(inStock);
+    }
+  }
 }
