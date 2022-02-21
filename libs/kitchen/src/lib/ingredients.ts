14914b0b003b86a71dbd9a2b8685e56b78eac0fa
Sergio Brito <sergiobritor@gmail.com>
2022-02-20 23:22:59 -0500

    feat(05-02): add method to verify if a dish is doable or not

---
+++ libs/kitchen/src/lib/ingredients.ts
@@ -0,0 +1,42 @@
+import { Dish, Ingredient } from '@yfx-data-models';
+
+export const ingredients: Ingredient[] = [
+  {
+    name: 'pescado',
+    dish: { name: 'ceviche' },
+    inStock: true
+  },
+  {
+    name: 'limón',
+    dish: { name: 'ceviche' },
+    inStock: false
+  },
+  {
+    name: 'cebolla',
+    dish: { name: 'ceviche' },
+    inStock: false
+  },
+  {
+    name: 'tortillas',
+    dish: { name: 'tacos' },
+    inStock: true
+  }
+];
+
+export function verify(dish: Dish): {
+  valid: boolean;
+  missedIngredients: string[];
+} {
+  const dishIngredients = ingredients.filter(
+    (ingredient) => ingredient.dish.name === dish.name && !ingredient.inStock
+  );
+
+  const missedIngredients = dishIngredients.map(
+    (ingredient) => ingredient.name
+  );
+
+  return {
+    valid: missedIngredients.length > 0 ? false : true,
+    missedIngredients
+  };
+}
