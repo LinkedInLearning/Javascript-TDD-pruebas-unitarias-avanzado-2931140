d37ac7734813d5524daa57db40b5a5ecc35540e9
Sergio Brito <sergiobritor@gmail.com>
2022-02-21 00:51:50 -0500

    test(05-02): use the Monkey Patching technique

--- libs/kitchen/src/lib/ingredients.spec.ts
+++ libs/kitchen/src/lib/ingredients.spec.ts
@@ -1,5 +1,48 @@
-describe('ingredients', () => {
-  it.todo('should get invalid dish', () => {});
+import { verify } from './ingredients';
+import { dishMock } from './mocks/dishes.mocks';
 
-  it.todo('should get a valid dish', () => {});
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const utils = require('./ingredients');
+
+describe('ingredients', () => {
+  it('should get invalid dish', () => {
+    const expectedVerification = {
+      missedIngredients: ['limón', 'cebolla'],
+      valid: false
+    };
+
+    expect(verify(dishMock)).toEqual(expectedVerification);
+  });
+
+  it('should get a valid dish', () => {
+    const originalIngredients = utils.ingredients;
+
+    utils.ingredients = [
+      {
+        name: 'pescado',
+        dish: { name: 'ceviche' },
+        inStock: true
+      },
+      {
+        name: 'limón',
+        dish: { name: 'ceviche' },
+        inStock: true
+      },
+      {
+        name: 'cebolla',
+        dish: { name: 'ceviche' },
+        inStock: true
+      }
+    ];
+
+    const expectedVerification = {
+      missedIngredients: [],
+      valid: true
+    };
+
+    expect(verify(dishMock)).toEqual(expectedVerification);
+    utils.ingredients; //?
+    utils.ingredients = originalIngredients;
+    utils.ingredients;
+  });
 });
